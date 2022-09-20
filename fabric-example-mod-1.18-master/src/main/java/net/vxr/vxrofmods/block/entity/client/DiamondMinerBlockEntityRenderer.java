17906771e2f6926067cc616ce1768073162a254a
vxrOfGitHub <vxrzweifelter.spam@outlook.de>
2022-09-20 17:38:03 +0200

    Fragment Holder done

--- fabric-example-mod-1.18-master/src/main/java/net/vxr/vxrofmods/block/entity/client/DiamondMinerBlockEntityRenderer.java
+++ fabric-example-mod-1.18-master/src/main/java/net/vxr/vxrofmods/block/entity/client/DiamondMinerBlockEntityRenderer.java
@@ -10,6 +10,7 @@ import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3f;
 import net.minecraft.world.LightType;
